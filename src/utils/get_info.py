23cb4ff0275f82830e2a4a24b1d6628d2367259e
muyu42 <74955143+muyu42@users.noreply.github.com>
2024-02-14 22:44:51 +0800

    Update get_info.py

--- src/utils/get_info.py
+++ src/utils/get_info.py
@@ -24,6 +24,8 @@ import json
 import faiss
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import Dataset
+#from torch.nn.utils.rnn import pad_sequence
+
 
 class ModelInfoRetriever:
     def __init__(self, model_name_or_path, data_path):
@@ -35,6 +37,33 @@ class ModelInfoRetriever:
         self.dataset = Dataset.from_json(data_path)#或者改成dataloader
         #TODO  判断传入是str 还是模型，如果传入str，按照上面进行加载，，如果传入模型。直接赋值给self#这一步方便其他地方调用
 
+        # 注意数据的索引 和保存的 几个文件要有对应关系？开一个东西记录映射？
+    def _get_gradients(self, input_text):
+        '''
+        在实际应用中，这个保存梯度的方法也可以进一步优化和定制化。
+        例如，可以只保存特定层的梯度，
+        或者以一定的格式来优化存储（例如，稀疏矩阵格式 比如lora）。
+        此代码提供了一个基本的模板，根据需求，可以进行进一步的调整。
+        '''
+        # Set model to training mode to compute gradients
+        self.model.train()
+        
+        inputs = self.tokenizer(input_text, return_tensors='pt')
+        outputs = self.model(**inputs, output_hidden_states=True)
+        logits = outputs.logits
+
+        # Calculate loss
+        shift_labels = inputs["input_ids"][..., 1:].contiguous()
+        loss = torch.nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), shift_labels.view(-1))
+        
+        # Compute gradients
+        self.model.zero_grad()  # Reset gradients
+        loss.backward()  # Calculate new gradients
+
+        # Extract gradients and detach them
+        gradients = {name: param.grad.detach() for name, param in self.model.named_parameters() if param.grad is not None}
+
+        return gradients
     def _get_embeddings_and_ppl(self, input_text):
         inputs = self.tokenizer(input_text, return_tensors='pt')
         with torch.no_grad():
@@ -65,15 +94,20 @@ class ModelInfoRetriever:
             embedding, ppl = self._get_embeddings_and_ppl(input_text)
             embeddings_collection.append(embedding.cpu().numpy())
             ppls.append(ppl)
+            all_gradients.append({k: v.cpu().numpy() for k, v in gradients.items()})
 
-      
 
-        # Save embeddings and PPL locally
+
+        # Save embeddings, PPL, and gradients locally
         torch.save(torch.stack(embeddings_collection), f'{output_dir}/embeddings.pt')
         with open(f'{output_dir}/ppls.jsonl', 'w') as f:
             for item in ppls:
                 f.write(json.dumps(item) + '\n')
-
+        with open(f'{output_dir}/gradients.jsonl', 'w') as f:
+            for grad_dict in all_gradients:
+                grad_dict_serializable = {k: v.tolist() for k, v in grad_dict.items()}
+                f.write(json.dumps(grad_dict_serializable) + '\n')   
+                
         ## If you want to use FAISS for saving embeddings:
         #embeddings_matrix = torch.stack(embeddings_collection).view(-1, embedding.size(-1)).numpy()
         #index = faiss.IndexFlatL2(embeddings_matrix.shape[1])
